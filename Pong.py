e0aae05884f5cc3d3988547c4b5acd8e89121f13
Abubakir <132080358+Mock405@users.noreply.github.com>
2023-05-12 20:18:31 +0500

    Update Pong.py
    я добавил отскок и проигрышь

--- Pong.py
+++ Pong.py
@@ -1,14 +1,3 @@
-from pygame import *
-win_width = 700
-win_height = 500
-window = display.set_mode((win_width,win_height))
-display.set_caption('Pong')
-background = transform.scale(image.load('background.jpg'),(win_width,win_height))
-clock = time.Clock()
-class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, width, height, player_speed):
-         super().__init__()
-         self.image = transform.scale(image.load(player_image), (width, height))
          self.speed = player_speed
          self.rect = self.image.get_rect()
          self.rect.x = player_x
